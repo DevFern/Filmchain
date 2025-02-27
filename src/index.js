053810f6e84610093da1c1937e50a68c26ed43a3
DevFern <fernandes_2@hotmail.com>
2025-02-27 11:50:17 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WalletProvider } from './contexts/WalletContext';
-import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <WalletProvider>
-        <App />
-      </WalletProvider>
-    </ErrorBoundary>
+    <WalletProvider>
+      <App />
+    </WalletProvider>
   </React.StrictMode>
 );
